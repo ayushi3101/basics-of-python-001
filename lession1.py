5604572d3a9dde9323ecdf34d7e434182b3899f6
ayushi3101 <163976187+ayushi3101@users.noreply.github.com>
2024-03-19 13:14:38 +0530

    Add files via upload

---
+++ lession1.py
@@ -0,0 +1,27 @@
+print("hello world")
+
+print("Hi, what's your name?")
+name= input()
+
+print("Hi,",name,"it's nice to meet you!")
+
+print("how old are you?")
+age= int(input())
+print("I see, you are", age, "years old")
+
+print("so where do you live?")
+place = input()
+print("oh! ",place,"I really like that place")
+
+print("Tell me 4 of your favorite beaches in Goa")
+beach1 = input();
+beach2 = input();
+beach3 = input();
+beach4 = input();
+places = [beach1,beach2,beach3,beach4]
+
+print("I see, so these are your favorite beaches in goa")
+print(places[0])
+print(places[1])
+print(places[2])
+print(places[3])
